0384ebdeccd8a8d413c2c88333478563a3abc4a8
Himanshu Saini <himanshusaini2705@gmail.com>
2025-11-11 01:36:38 +0530

    Update main.c

--- Array/main.c
+++ Array/main.c
@@ -4,9 +4,15 @@
 int main() {
     int a[] = {1,2,3,4,5,6,7,8,9};
     int len = sizeof(a)/sizeof(int);
+
+    // Reverse Function Use
     reverse(a,len);
     for (int i = 0; i < len; i++)
     {
         printf("%d ", a[i]);
-    } 
+    }
+
+    // Linear Search Function Use
+    printf("\n%d ", LinearSearch(a,len,5));
+
 }
